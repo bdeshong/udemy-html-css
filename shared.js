f13d2538244a2ddab33cb89ef0319f13350ca088
Brian DeShong <brian@deshong.net>
2019-11-03 00:25:25 -0400

    Completed!

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var modalNegativeButton = document.querySelector('.modal__action--negative');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
@@ -52,3 +53,15 @@ function closeModal() {
         backdrop.style.display = 'none';
     }, 200);
 }
+
+ctaButton.addEventListener('animationstart', function (event) {
+    console.log('Animation started', event);
+});
+
+ctaButton.addEventListener('animationend', function (event) {
+    console.log('Animation ended', event);
+});
+
+ctaButton.addEventListener('animationiteration', function (event) {
+    console.log('Animation iteration', event);
+});
